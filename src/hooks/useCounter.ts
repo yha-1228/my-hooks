9763eb682028701648516c25552a7d5d11886b7c
長谷川 裕大 <gssbtsm999@gmail.com>
2021-05-23 05:48:13 +0900

    Implement useCounter

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,3 +1,13 @@
-function useCounter() {}
+import { useState } from 'react'
+
+function useCounter(initialCount: number) {
+  const [count, setCount] = useState(initialCount)
+
+  return {
+    count,
+    increment: () => setCount((count) => count + 1),
+    decrement: () => setCount((count) => count - 1),
+  }
+}
 
 export default useCounter
